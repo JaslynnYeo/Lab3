99f792853bae89b3d3420a2c18239f0e7f3944e9
jaslynyeo <jaslynykl.23@ichat.sp.edu.sg>
2024-05-12 19:04:49 +0800

    added assert

--- test_price_info.py
+++ test_price_info.py
@@ -1,3 +1,4 @@
+import price_info
 
 def test_total_cost_shopping():
     price_list={'apple' : 1., 'orange':2}
@@ -8,15 +9,18 @@ def test_total_cost_shopping():
     for key in price_list.keys():
         if key in quantity_list:
             total_cost = quantity_list[key]*price_list[key] 
-            x += total_cost  
+            x += total_cost 
+
+    assert(test== x) 
 
 def test_cost_of_fruits():
-    price_list={'apple' : 1., 'orange':2}
-    quantity_list= {'apple': 1, 'orange':2}
-    fruits = 0
-    for key in price_list.keys():
-        if fruits == quantity_list[key]: 
-            cost = fruits*price_list[key]
+    fruits = "apple"
+    quantity = 10
+    actual_res = price_info.cost_of_fruits(fruits, quantity)
+    test = 12
+    assert(test == actual_res)
+
+
 
 
 
